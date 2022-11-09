b8381552196f20abe3d6bf939a134d554e086666
Pokiledaa <parsaoski.po@gmail.com>
2022-11-09 12:44:57 +0330

    Adde The Command Line for get user and code cleaning

---
+++ consol.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser
+
+class Argument :
+    def __init__(self) :
+        self.parser= ArgumentParser(description="<<Welcome TO the Xray-tools>>")
+        self.sub_parser = self.parser.add_subparsers(dest="command")
+
+        self.get_user_parser = self.sub_parser.add_parser("get",help="Returning The User From Config File ")
+        self.get_user_parser.add_argument("email",type=str)
+        self.get_user_parser.add_argument("-d","--domain",type=str)
+        self.get_user_parser.add_argument("-n","--name",type=str)
+
+        self.add_user_parser = self.sub_parser.add_parser("add",help="Adding The user to The Config File ")
+
+
+    def start(self):
+        # here We Parse Our Args             
+        self.args =  self.parser.parse_args()
---
+++ xray-manager.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+from stricker_watcher import StrickerWatcher
+from user_managment import ClientHandler
+from consol import Argument
+import json
+
+
+class Config:
+    def __init__(
+        self,
+        conf_dir:str
+    )->None :
+
+        self.conf_dir = conf_dir
+        
+        self.conf_file = open(self.conf_dir,"r")
+        self.conf_dict = json.loads(self.conf_file.read()) 
+        self.xray_conf = self.conf_dict["XRAY_CONF"]
+
+        self.conf_file.close()
+
+
+class XrayHandler:
+    def __init__(self):
+        self.system_conf = Config("conf.json")
+        self.client_handler = ClientHandler(self.system_conf.xray_conf)
+        self.arguments = Argument()
+
+
+    def consol_start(self):
+        self.arguments.start()
+        # Here we Parse The Get User
+        if self.arguments.args.command :
+            command = self.arguments.args.command
+
+            if command == "get":
+                if self.arguments.args.domain == None :
+                    print("error : Please Enter The Domain name with |-i domain-name|")
+                    exit()
+                if self.arguments.args.name == None :
+                    print("error : Please Enter The vpn name  with |-n vpn-name|")
+                    exit()
+
+                email:str = self.arguments.args.email
+                domain:str =  self.arguments.args.domain
+                vpn_name:str =  self.arguments.args.name
+                vpn_name = vpn_name.replace(" ","+")
+                profile = self.client_handler.get_client_profile(email)
+                if not profile :
+                    print("Error : No user Found")
+                else : 
+                    print("\r\n\r\n--------------------------------------------------Client URL-----------------------------------------------------------------")
+                    url = self.client_handler.get_client_url(email,domain,vpn_name)
+                    print(url)
+                    print("----------------------------------------------------------------------------------------------------------------------------\r\n\r\n")
+                    self.client_handler.get_client_qrcode(email,url)
+                    
+
+
+            elif command == "add":
+                pass
+            
+
+
+
+
+def main():    
+    consol = XrayHandler()
+    consol.consol_start()
+
+if __name__ == "__main__":
+    main()
