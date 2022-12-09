a6b0c5da5c175ae124cdc6f4ffc04e5b48328671
pokiledaa <parsaoski.po@gmail.com>
2022-12-09 12:35:06 +0000

    added the new requirment

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 
 python3 get-pip.py
 
-pip install image qrcode psutil requests
+pip install image qrcode psutil requests python-docx
 
 python3 setup.py
