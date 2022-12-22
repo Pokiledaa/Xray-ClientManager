502a39428e9f2daccbbc2916910335f87b51122a
pokiledaa <parsaoski.po@gmail.com>
2022-12-22 06:58:51 +0000

    commit for generated dosx and git ignore

--- doc_generator.py
+++ doc_generator.py
@@ -18,14 +18,14 @@ class DocGenerator:
         doc = Document()
         doc.add_heading(client_identity).paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
         doc.add_paragraph('%s           %s' %(uuid,self.hostname), style='Intense Quote').paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER        
-        doc.add_paragraph("VMESS CDN").paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        doc.add_picture(f"{Directories.GENERATED_OUTPUT}/{client_identity}_CDN.PNG",width=Inches(6)) 
+        doc.add_paragraph("VLESS TCP").paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        doc.add_picture(f"{Directories.GENERATED_OUTPUT}/{client_identity}_VLESS-TCP.PNG",width=Inches(6)) 
         doc.add_page_break()
-        doc.add_paragraph("VMESS TLS").paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        doc.add_picture(f"{Directories.GENERATED_OUTPUT}/{client_identity}_TLS.PNG",width=Inches(6))
+        doc.add_paragraph("VLESS TCP XTLS").paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        doc.add_picture(f"{Directories.GENERATED_OUTPUT}/{client_identity}_VLESS-TCP-XTLS.PNG",width=Inches(6))
         doc.add_page_break()
-        doc.add_paragraph("VMESS TLS CDN").paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        doc.add_picture(f"{Directories.GENERATED_OUTPUT}/{client_identity}_TLS-CDN.PNG",width=Inches(6))   
+        doc.add_paragraph("VLESS WS TLS").paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        doc.add_picture(f"{Directories.GENERATED_OUTPUT}/{client_identity}_VLESS-WS-TLS.PNG",width=Inches(6))   
         doc.save(f"{Directories.GENERATED_DOCX}/{client_identity}.docx")
     
 
